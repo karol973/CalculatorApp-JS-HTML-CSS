294384a11f29232d176f64d3533aaf817fe21bd8
karol973 <karolek973@wp.pl>
2022-07-03 10:15:54 +0200

    Add functionality of calculations history

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const currentNumber = document.querySelector('.currentNumber');
 const previousNumber = document.querySelector('.previousNumber p');
 const mathSign = document.querySelector('.mathSign');
-const operatorBtn = document.querySelectorAll('operator');
+const operatorBtn = document.querySelectorAll('.operator');
 const numberBtns = document.querySelectorAll('.number')
 const equalBtn= document.querySelector('.equals');
 const clearBtn = document.querySelector('.clear');
@@ -13,40 +13,81 @@ let result='';
 
 
 function displayNumbers(){
-if(this.textContent==='.' && currentNumber.includes('.')) return; /*jezeli klikniemy na . i liczba zawiera juz kropke - to zwroc funkcje(nie rob nic)*/
-if(this.textContent==='.' && currentNumber.innerHTML ==='')return currentNumber.innerHTML='.0'; /*jezeli kliknelismy na kropke to dodaj 0 za kropka*/
+
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return; /*jezeli klikniemy na . i liczba zawiera juz kropke - to zwroc funkcje(nie rob nic)*/
+    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0' ;/*jezeli kliknelismy na kropke to dodaj 0 za kropka*/
 currentNumber.innerHTML += this.textContent; /*w dodajemy kolejną cyfre , aby móc zapisac liczbe */
 }
-function operate() {
-if(currentNumber.innerHTML==='' && currentNumber.textContent==='-' ){
-    currentNumber.innerHTML='-';
-    return;
+function operate () {
+    if(currentNumber.innerHTML === '' && this.textContent ==='-'){
+        currentNumber.innerHTML = '-';
+        return;
+    }
+
+
+    else if (currentNumber.innerHTML === '') {
+        return;
+    }
+
+    if(mathSign.innerHTML !== '') {
+        showResult();
+    }
+    previousNumber.innerHTML = currentNumber.innerHTML;
+    mathSign.innerHTML = this.textContent;
+    currentNumber.innerHTML ='';
 }
-else if(currentNumber.innerHTML='') return;
 
+function showResult() {
+    if (previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return; /*jesli nie podamy drugiej liczby to zwróci pierwsza*/
 
-}
-if (mathSign.innerHTML !==''){
-    showResult();
+    let a = Number(currentNumber.innerHTML);
+    let b = Number(previousNumber.innerHTML);
+    let operator = mathSign.innerHTML;
 
-}
-previousNumber.innerHTML = currentNumber.innerHTML;
-mathSign.innerHTML=this.textContent;
-currentNumber.innerHTML='';
-function showResult(){
-if(previousNumber.innerHTML===''|| currentNumber.innerHTML ==='' ) return;
+    switch (operator) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case 'x':
+            result = a * b;
+            break;
+        case ':':
+            result = b / a;
+            break;
+        case '2^':
+            result = b ** a;
+            break;
 
-let a = currentNumber.innerHTML;
-let b = previousNumber.innerHTML;
-let operator = mathSign.innerHTML;
+    }
+    addToHistory();
+    historyBtn.classList.add('active');
+    currentNumber.innerHTML = result;
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
+
 }
 function clearScreen(){
+currentNumber.innerHTML='';
+    previousNumber.innerHTML = '';
+    result='';
+mathSign.innerHTML='';
+}
+function clearHistory(){
+    calculatorHistory.textContent='';
 
 }
 
 
 
-
+function addToHistory(){
+const historyItem = document.createElement('li');
+historyItem.innerHTML=`${currentNumber.innerHTML} ${mathSign.innerHTML} ${previousNumber.innerHTML} = ${result}`;
+historyItem.classList.add('history-item');
+calculatorHistory.appendChild(historyItem);
+}
 
 
 
@@ -59,4 +100,7 @@ operatorBtn.forEach(btn=>{
 
 equalBtn.addEventListener('click',showResult);
 clearBtn.addEventListener('click', clearScreen);
-numberBtns.forEach((btn => btn.addEventListener('click', displayNumbers)));
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', displayNumbers)
+    });
+historyBtn.addEventListener('click', clearHistory);
